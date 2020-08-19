dde7570df9e32c006d77367f855caadb29b4ebed
vitorararuna <contato.vitorararuna@gmail.com>
2020-08-19 10:07:09 -0300

    Desafio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+
+  const [repo, setRepo] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepo(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `App MongoDB com Node.js ${Date.now()}`,
+      url: 'www.meurepo/abc',
+      techs: ["ReactJS", "React Native", "Node.Js"]
+    })
+
+    const newRepo = response.data;
+    setRepo([...repo, newRepo]);
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    const response = await api.delete(`repositories/${id}`)
+    setRepo(repo.filter(repo => repo.id !== id));
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repo.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
+
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+        </button>
+          </li>
+        ))
+        }
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
